79ae351e33e1abc00af8a0dcdeb0c05b4817dd43
Ananda Prayoga <87570063+anandaprayoga@users.noreply.github.com>
2022-06-02 20:29:03 +0700

    Create Area-and-Circumference-of-Circle.py

---
+++ Area-and-Circumference-of-Circle.py
@@ -0,0 +1,9 @@
+radius = eval(input("Insert radius: "))
+
+pi = 3.14159
+
+area = (radius ** 2) * pi
+circumference = 2 * radius * pi
+
+print(f"The area of circle is {area:.2f} square meters")
+print(f"The circumference of circle is {circumference:.2f} centimeters")
